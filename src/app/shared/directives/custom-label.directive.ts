633a78ac398b8de9266194daee23db4754317673
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-14 14:01:54 -0500

    directive input - change color of host

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]'
@@ -6,16 +6,29 @@ import { Directive, ElementRef, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color( value: string ) {
+    // console.log({ color: value });
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor( private el: ElementRef<HTMLElement> ) {
-    console.log('Constructor de la directiva');
-    console.log(el);
+    // console.log('Constructor de la directiva');
+    // console.log(el);
     this.htmlElement = el;
-    this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
+    // this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
   }
 
   ngOnInit(): void {
     console.log('Directiva - NgOnInit ');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if ( !this.htmlElement ) return;
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 
 }
