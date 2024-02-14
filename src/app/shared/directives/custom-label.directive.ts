9960fb8f89a5cd13ae92af09cde9f8d454ec2b12
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-14 13:09:01 -0500

    add custom directvie - custom label

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]'
+})
+export class CustomLabelDirective implements OnInit {
+
+  private htmlElement?: ElementRef<HTMLElement>;
+
+  constructor( private el: ElementRef<HTMLElement> ) {
+    console.log('Constructor de la directiva');
+    console.log(el);
+    this.htmlElement = el;
+    this.htmlElement.nativeElement.innerHTML = 'Hola Mundo';
+  }
+
+  ngOnInit(): void {
+    console.log('Directiva - NgOnInit ');
+  }
+
+}
