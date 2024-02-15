59440889eb245a1dc9a133e34abb7ffe1c519773
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-15 17:19:03 -0500

    add signals module & lazy loading in app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'products',
-    loadChildren : () => import('./products/products.module').then( m => m.ProductsModule )
+    loadChildren : () => import('./products/products.module').then(m => m.ProductsModule)
+  },
+  {
+    path: 'signals',
+    loadChildren: () => import('./signals/signals.module').then(m => m.SignalsModule)
   },
   {
     path: '**',
