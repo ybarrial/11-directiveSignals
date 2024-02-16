7cef1b6b7be7a625d5c65b5671f710538419ce45
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-16 18:42:09 -0500

    add counter-page signal & computed signals

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
+const name = signal('Bl4ckP4r4d3');
 
 @Component({
   templateUrl: './counter-page.component.html',
   styleUrls: ['./counter-page.component.css']
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+  // SENIAL COMPUTADA de SOLO LECTURA
+  public squareCounter = computed( (): number => this.counter() * this.counter());
+
+  constructor() {
+    console.log(name());
+    
+  }
+
+  increaseBy( value: number ) {
+    this.counter.update( current => current + value )
+  }
 
 }
