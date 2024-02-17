2ba76ed22948032fadf70306f9ebbec553d90161
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-16 19:51:56 -0500

    manage of errors & computed properties

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, signal } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { User } from '../../interfaces/user-request.interface';
 
@@ -14,6 +14,11 @@ export class UserInfoPageComponent implements OnInit {
   public currentUser = signal<User | undefined>(undefined);
   public userWasFound = signal(true);
 
+  public fullName = computed<string>( () => {
+    if ( !this.currentUser ) return 'Usuario no encontrado';
+    return `${ this.currentUser()?.first_name } ${ this.currentUser()?.last_name }`;
+  });
+
 
   ngOnInit(): void {
     this.loadUser( this.userId() );
@@ -26,9 +31,19 @@ export class UserInfoPageComponent implements OnInit {
     this.currentUser.set(undefined);
 
     this.usersService.getUserById( id )
-      .subscribe( user => {
-        console.log({ user });
-        this.currentUser.set( user )
+      // .subscribe( user => {
+      //   console.log({ user });
+      //   this.currentUser.set( user )
+      // })
+      .subscribe({
+        next: (user) => {
+          this.currentUser.set(user);
+          this.userWasFound.set(true);
+        },
+        error: () => {
+          this.userWasFound.set(false);
+          this.currentUser.set(undefined);
+        }
       })
   }
 
