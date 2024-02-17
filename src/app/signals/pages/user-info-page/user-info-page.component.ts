5defe6c557488fe22b94217a75fffe8bf1014d3e
Your Name <yefferson.barrial@avivel.com.pe>
2024-02-16 19:23:52 -0500

    add user info with signals & properties

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
+
+  private usersService = inject(UsersService);
+  public userId = signal(1);
+
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+
+  ngOnInit(): void {
+    this.loadUser( this.userId() );
+  }
+
+  loadUser( id: number ) {
+    if ( id <= 0 ) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+
+    this.usersService.getUserById( id )
+      .subscribe( user => {
+        console.log({ user });
+        this.currentUser.set( user )
+      })
+  }
 
 }
